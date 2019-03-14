ee45e83c8c502ac2f09731de85ec27228c045d74
nakkasoft <nakka12@empal.com>
2019-03-14 22:13:20 +0900

    Update order logic and save last id

---
+++ precache-manifest.32f3a6c2fd6d2ef9d3bf8b618f3bc40b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86494e9c6d32fb67be2b",
+    "url": "/todo-list/static/js/runtime~main.86494e9c.js"
+  },
+  {
+    "revision": "12ab022137085d70deb9",
+    "url": "/todo-list/static/js/main.12ab0221.chunk.js"
+  },
+  {
+    "revision": "7ea9dbe65b1a0a30a6c2",
+    "url": "/todo-list/static/js/2.7ea9dbe6.chunk.js"
+  },
+  {
+    "revision": "12ab022137085d70deb9",
+    "url": "/todo-list/static/css/main.ac5f5a97.chunk.css"
+  },
+  {
+    "revision": "1ddec8d8334537026c84da767fa6a8cf",
+    "url": "/todo-list/index.html"
+  }
+];
